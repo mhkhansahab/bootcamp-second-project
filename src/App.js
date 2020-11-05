37c2fc3c42785896f0aceafffe28ac0445685159
Muhammad Hamza Khan <hamza.khansahab@gmail.com>
2020-11-05 12:56:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Hello to React Developers
+     Hello to React Developers!
     </div>
   );
 }
